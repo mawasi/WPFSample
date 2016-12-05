59a3bcfca70f32ee91da800b19eab5031072cbb0
mawasi <mawasi99@hotmail.com>
2016-12-06 04:57:38 +0900

    タイマー付き時計完成

--- StopWatch/MainWindow.xaml.cs
+++ StopWatch/MainWindow.xaml.cs
@@ -17,6 +17,21 @@ using System.Windows.Shapes;
 using System.ComponentModel;
 
 
+/*
+ 参考URL
+WPF4.5入門 その40 「Popup、ToolTip、TextBox、Image、MediaElementコントロール」
+http://blog.okazuki.jp/entry/2014/08/16/221447
+
+WPFでタスクトレイにアイコンを表示する
+http://csfun.blog49.fc2.com/blog-entry-93.html
+
+タスクトレイにアイコンとバルーンを表示する
+https://codezine.jp/article/detail/421
+
+XAML+C#でWPF NotifyIconを使ってオリジナル通知アイコンを表示する
+http://qiita.com/Oichan/items/68a5fd339e12193b3ca4
+*/
+
 namespace StopWatch
 {
 	/// <summary>
@@ -28,6 +43,12 @@ namespace StopWatch
 		/// 設定した時間後に通知する用のタイマー
 		/// </summary>
 		System.Windows.Threading.DispatcherTimer mTimer = new System.Windows.Threading.DispatcherTimer();
+
+		/// <summary>
+		/// タスクトレイ通知アイコン
+		/// Solution ExplorerのReferencesからadd ReferenceでSystem.WIndows.Formsしてやる
+		/// </summary>
+		System.Windows.Forms.NotifyIcon		mNotifyIcon;
 
 		public MainWindow()
 		{
@@ -43,16 +64,16 @@ namespace StopWatch
 			TimerButton.DataContext = timervm;
 			HourList.DataContext = timervm;
 			MinutesList.DataContext = timervm;
-#if false
-			HourList.ItemsSource = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
-			HourList.SelectedIndex = 0;
-			string[] minutes = new string[60];
-			for(int i = 0; i < minutes.Length; i++) {
-				minutes[i] = i + " Min";
-			}
-			MinutesList.ItemsSource = minutes;
-			MinutesList.SelectedIndex = 0;
-#endif
+
+			mNotifyIcon = new System.Windows.Forms.NotifyIcon();
+			mNotifyIcon.Text = "Timer";
+			mNotifyIcon.Icon =  new System.Drawing.Icon("Watch.ico");
+			mNotifyIcon.Visible = true;
+			mNotifyIcon.BalloonTipText = "タイマーが終了しました";
+			mNotifyIcon.BalloonTipTitle = "Timer";
+			mNotifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+
+
 		}
 
 
@@ -65,12 +86,51 @@ namespace StopWatch
 
 		}
 
+		/// <summary>
+		/// タイマー用ボタンクリック時イベント
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		private void TimerButton_Click(object sender,RoutedEventArgs e)
 		{
 			Button button = sender as Button;
 			TimerDataViewModel vm = button.DataContext as TimerDataViewModel;
 
 			vm.TimerOn = (vm.TimerOn ? false : true);
+
+			// タイマー設定
+			mTimer.Tick += TimeOverEvent;
+			int hour = HourList.SelectedIndex;
+			int minutes = MinutesList.SelectedIndex;
+			mTimer.Interval = new TimeSpan(0, hour, minutes, 0);
+			mTimer.Start();
+		}
+
+
+		/// <summary>
+		/// タイマー終了時イベント
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		void TimeOverEvent(object sender, EventArgs e)
+		{
+			TimerDataViewModel vm = TimerButton.DataContext as TimerDataViewModel;
+
+			vm.TimerOn = false;
+			mTimer.Stop();
+	//		MessageBox.Show("Time Over", "Timer Notify");
+			mNotifyIcon.ShowBalloonTip(1000);
+		}
+
+		/// <summary>
+		/// アプリ終了時イベント(ウィンドウ終了時イベント)
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		void ClosingWindow(object sender, CancelEventArgs e)
+		{
+			// ちゃんと明示的にDisposeしてやらないとタスクバーに残る
+			mNotifyIcon.Dispose();
 		}
 	}
 
@@ -159,10 +219,10 @@ namespace StopWatch
 		public TimerData()
 		{
 			for(int i = 0; i < 24; i++){
-				HourList.Add(i + "Hour");
+				HourList.Add(i + " Hour");
 			}
 			for(int i = 0; i < 60; i++) {
-				MinutesList.Add(i + "Min");
+				MinutesList.Add(i + " Min");
 			}
 		}
     }
